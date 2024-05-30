150ee335f22430b95b83daf40b9e37435d2ccffe
Alethepata <alessia.patane@gmail.com>
2024-05-30 15:17:45 +0200

    add lowercase

--- test/slug.test.js
+++ test/slug.test.js
@@ -1,9 +1,18 @@
 const { expect } = require('@jest/globals');
 
 const createSlug = () => {
-    return ''
+    let slug = 'slug';
+
+    if (typeof slug !== 'string') { 
+        return slug.toString().toLowerCase();
+    }
+    return slug.toLowerCase();   
 }
 
 test('createSlug dovrebbe ritornare una stringa', () => {
     expect(typeof createSlug()).toBe('string');
+})
+
+test('createSlug dovrebbe ritornare una stringa in lowercase', () => {
+    expect(createSlug()).toBe(createSlug().toLowerCase());
 })
