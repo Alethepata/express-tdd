50de5c30b01cae3080ed9759631b1859f8b89f7a
Alethepata <alessia.patane@gmail.com>
2024-05-30 15:45:43 +0200

    add increase

--- test/slug.test.js
+++ test/slug.test.js
@@ -1,11 +1,24 @@
 const { expect } = require('@jest/globals');
 
+const posts = require('../db/posts.json');
+const slugs = posts.map(post => post.slug);
+
+console.log(slugs);
+
 const createSlug = () => {
-    let slug = 'slug prova';
+    let slug = 'ciambellone';
 
     if (typeof slug !== 'string') { 
         return slug.toString().toLowerCase().replace(' ', '-');
     }
+
+    let counter = 1;
+
+    while (slugs.includes(slug)) {
+        slug = `${slug}-${counter}`;
+        counter++;
+    }
+
     return slug.toLowerCase().replace(' ', '-');   
 }
 
@@ -19,4 +32,8 @@ test('createSlug dovrebbe ritornare una stringa in lowercase', () => {
 
 test('createSlug dovrebbe ritornare una stringa con gli spazi sostituiti da -', () => {
     expect(createSlug()).toBe(createSlug().replace(' ', '-'));
+})
+
+test('createSlug dovrebbe incrementare di 1 lo slug quando esiste già', () => {
+    expect(slugs).not.toContain(createSlug());
 })
